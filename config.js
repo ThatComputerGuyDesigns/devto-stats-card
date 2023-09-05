5b2fb7eea118ad27e8c3adf88ef5a001683de028
ThatComputerGuyDesigns <144140385+ThatComputerGuyDesigns@users.noreply.github.com>
2023-09-05 10:09:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   port: process.env.PORT || 3000,
-  blogUrl: "dev.to/amrelmohamady",
+  blogUrl: "dev.to/thatcomputerguy",
   dashboardUrl: "https://dev.to/dashboard",
   cookieHeader: process.env.COOKIE_HEADER
 };
